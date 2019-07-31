2e516dbc513becb8b05936ae35b4327609ab284c
Bilal Karatas <bilalkaratash@gmail.com>
2019-07-30 22:39:43 -0400

    asda

--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -4,6 +4,9 @@ public class FirstTest {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Test");
+        System.out.println("safaf");
+
+
     }
 }
 
